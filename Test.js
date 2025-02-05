e03f9d451cac004a55ef3cbcaa4c3af706497f9c
Ray-Shubham <raysubham555@gmail.com>
2025-02-05 21:23:29 +0530

    String to Uppercase

--- Test.js
+++ Test.js
@@ -1,14 +1,52 @@
-var x= 1;
-a();
-b();
-console.log(x)
+// var x= 1;
+// a();
+// b();
+// console.log(x)
 
-function a() {
-    var x=10;
-    console.log(x);
-}
+// function a() {
+//     var x=10;
+//     console.log(x);
+// }
 
-function b() {
-    var x=20
-    console.log(x);
+// function b() {
+//     var x=20
+//     console.log(x);
+// }
+
+// function a() {
+//     console.log("a called")
+// }
+
+// var b = function () {
+//     console.log("b called")
+// }
+// a();
+// b();
+
+// function xyz(param1) {
+//     console.log(param1)
+// }
+// function y() {
+//     console.log("y called")
+// }
+// xyz(y);
+
+// let c = function (x) {
+//     return function xyz() {
+
+//     }
+// }
+// console.log(c())
+
+
+
+setTimeout(() => {
+    console.log("Timer")
+}, 5000);
+function x(y) {
+    console.log("x");
+    y();
 }
+x(function y() {
+    console.log("y")
+})
--- string_first_letter_uppercase.js
+++ string_first_letter_uppercase.js
@@ -1,12 +1,53 @@
 // 7. Write a JavaScript program to change the string to title (first letter of every word should be in upper case)
 
-function capitalizeFirstLetterOfEachWord(str) {
-    const words = str.split(" ");
-    const capitalizedWords = words.map(word => {
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    });
-    return capitalizedWords.join(" ");
-}
-const inputString = "hello world this is a sample string";
-const capitalizedString = capitalizeFirstLetterOfEachWord(inputString);
-console.log("Capitalized string:", capitalizedString);
+// function capitalizeFirstLetterOfEachWord(str) {
+//     const words = str.split(" ");
+//     const capitalizedWords = words.map(word => {
+//         return word.charAt(0).toUpperCase() + word.slice(1);
+//     });
+//     return capitalizedWords.join(" ");
+// }
+// const inputString = "hello world this is a sample string";
+// const capitalizedString = capitalizeFirstLetterOfEachWord(inputString);
+// console.log("Capitalized string:", capitalizedString);
+
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// rl.question('Enter Your Name: ', (input) => {
+//     // Add input validation
+//     if (!input.trim()) {
+//         console.log('Name cannot be empty!');
+//     } else {
+//         console.log(`Your Name is: ${input.trim()}`);
+//     }
+//     rl.close();
+// });
+
+// // Handle the close event
+// rl.on('close', () => {
+//     console.log('Input process completed');
+//     process.exit(0);
+// });
+
+
+
+// const input = process.argv[2];
+// if (input) {
+//     console.log(`Your Name is: ${input}`);
+// } else {
+//     console.log('No input provided.');
+// }
+
+
+
+process.stdout.write('Enter Your Name: ');
+process.stdin.on('data', (data) => {
+    const input = data.toString().trim();
+    console.log(`Your Name is : ${input}`);
+    process.exit();
+});

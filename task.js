d78f31c5f4ed6bb5777aeed76686e58a6c253b68
Ray-Shubham <raysubham555@gmail.com>
2025-02-12 13:30:30 +0530

    onmouseover and onmouseout events

--- task.js
+++ task.js
@@ -1,16 +1,1 @@
-// function bulbOn() {
-//   document.getElementById("pic1").src = "./DOM/bulb_on.png";
-// }
 
-// function bulbOff() {
-//   document.getElementById("pic1").src = "./DOM/bulb_off.png";
-// }
-
-function changeBulb() {
-  let image = document.querySelector('#pic1');
-  if (image.src.match("bulb_on")) {
-    image.src = "./DOM/bulb_off.png";
-  } else {
-    image.src = "./DOM/bulb_on.png";
-  }
-}
